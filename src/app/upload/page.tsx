4a3c15506f1932e04b9f5246d45e3c2b9b569f22
ssolson <sterlingolson@yahoo.com>
2024-01-31 15:48:44 -0700

    AddRecipeForm

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <main className="antialiased min-h-screen mx-auto max-w-6xl pt-10">
       <div className="grid grid-cols-4 gap-4">
-        <AddForm />
+        <AddRecipeForm />
         {/* <ListSong /> */}
       </div>
     </main>
